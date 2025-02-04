6de86bb5f2fa1f98a0d83bbf0ca5b7d9441b8ae9
jo jungmin <cjm01115@naver.com>
2025-02-04 21:30:31 +0900

    upload_to_gemini 함수를 위로 빼어 최적화 작업 수행 15분마다 차트를 확인 하겟금함 추론 요청에서 오류가 날 경우 최대 3번 다시 시도도

--- main.py
+++ main.py
@@ -22,154 +22,159 @@ logging.basicConfig(
 
 load_dotenv()
 
+def upload_to_gemini(path, mime_type=None):
+    """Uploads the given file to Gemini.
+
+    See https://ai.google.dev/gemini-api/docs/prompting_with_media
+    """
+    file = genai.upload_file(path, mime_type=mime_type)
+    print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+    return file
+
 def round_price(price):
     """XRP/USDT 가격을 최소 정밀도(0.0001)에 맞게 반올림"""
     return round(float(price), 4)
 
 def automation():
-    try:
-        bybit = BybitUtils(True)
+    bybit = BybitUtils(True)
+    
+    # 현재 포지션 체크
+    current_position = bybit.get_positions()
+    # TODO 리팩토링 진행
+    # if len(current_position)>0:
+    #     logging.info("Active position exists, skipping trading cycle")
+    #     return
+    
+    current_orders = bybit.get_orders()
+    # if current_orders is not None and len(current_position)==0:
+    # # 포지션이 없으며 현재 오더가 있으면 동작
+    #   logging.info("Active position exists, cancle current orders")
+    #   bybit.cancle_orders()
         
-        # 현재 포지션 체크
-        current_position = bybit.get_positions()
-        # TODO 리팩토링 진행
-        # if len(current_position)>0:
-        #     logging.info("Active position exists, skipping trading cycle")
-        #     return
-        
-        current_orders = bybit.get_orders()
-        # if current_orders is not None and len(current_position)==0:
-        # # 포지션이 없으며 현재 오더가 있으면 동작
-        #   logging.info("Active position exists, cancle current orders")
-        #   bybit.cancle_orders()
-          
 
-        # Gemini API 설정
-        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    # Gemini API 설정
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-        def upload_to_gemini(path, mime_type=None):
-            """Uploads the given file to Gemini.
+    generation_config = {
+        "temperature": 0.7,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 65536,
+        "response_mime_type": "text/plain",
+    }
 
-            See https://ai.google.dev/gemini-api/docs/prompting_with_media
-            """
-            file = genai.upload_file(path, mime_type=mime_type)
-            print(f"Uploaded file '{file.display_name}' as: {file.uri}")
-            return file
+    model = genai.GenerativeModel(
+        model_name="gemini-2.0-flash-thinking-exp-01-21",
+        generation_config=generation_config,
+        system_instruction="You are a cryptocurrency trader\nYou are fully aware of the risks of cryptocurrencies and know the dangers of leverage\nPlease send us a picture of your chart and let us know if you are selling or buying\nYou need to set a clear direction at this point\nYou need to choose between watch, sell, buy, or position user\nPlease clearly tell us your purchase price (market or limit), stop loss, and take profit price. \If the position is contingent, you can only choose one of two options: watch and wait or close with a hold or stop\nIn the <Result> tag, indicate whether you want to buy, sell, watch and wait, or close. TP,SL etc... Please summarize the result and state what you will do with it.",
+    )
 
-        generation_config = {
-            "temperature": 0.7,
-            "top_p": 0.95,
-            "top_k": 64,
-            "max_output_tokens": 65536,
-            "response_mime_type": "text/plain",
-        }
+    # 차트 생성 및 가격 정보 획득
+    # TODO 비동기적으로 진행하여 빠르게 이미지를 업로드 할 수 있게 해야함
+    price_utils = bybit_utils("XRP/USDT", "1h", 100)
+    price_utils.plot_candlestick()
+    price_utils.set_timeframe("4h")
+    price_utils.plot_candlestick()
+    price_utils.set_timeframe("15m")
+    price_utils.plot_candlestick()
+    current_price = price_utils.get_current_price()
 
-        model = genai.GenerativeModel(
-            model_name="gemini-2.0-flash-thinking-exp-01-21",
-            generation_config=generation_config,
-            system_instruction="You are a cryptocurrency trader\nYou are fully aware of the risks of cryptocurrencies and know the dangers of leverage\nPlease send us a picture of your chart and let us know if you are selling or buying\nYou need to set a clear direction at this point\nYou need to choose between watch, sell, buy, or position user\nPlease clearly tell us your purchase price (market or limit), stop loss, and take profit price. \If the position is contingent, you can only choose one of two options: watch and wait or close with a hold or stop\nIn the <Result> tag, indicate whether you want to buy, sell, watch and wait, or close. TP,SL etc... Please summarize the result and state what you will do with it.",
-        )
+    files = [
+        upload_to_gemini("XRP_USDT_15m_candlestick.png", mime_type="image/png"),
+        upload_to_gemini("XRP_USDT_1h_candlestick.png", mime_type="image/png"),
+        upload_to_gemini("XRP_USDT_4h_candlestick.png", mime_type="image/png"),
+    ]
 
-        # 차트 생성 및 가격 정보 획득
-        # TODO 비동기적으로 진행하여 빠르게 이미지를 업로드 할 수 있게 해야함
-        price_utils = bybit_utils("XRP/USDT", "1h", 100)
-        price_utils.plot_candlestick()
-        price_utils.set_timeframe("4h")
-        price_utils.plot_candlestick()
-        price_utils.set_timeframe("15m")
-        price_utils.plot_candlestick()
-        current_price = price_utils.get_current_price()
-
-        files = [
-            upload_to_gemini("XRP_USDT_15m_candlestick.png", mime_type="image/png"),
-            upload_to_gemini("XRP_USDT_1h_candlestick.png", mime_type="image/png"),
-            upload_to_gemini("XRP_USDT_4h_candlestick.png", mime_type="image/png"),
+    print(f"Current Price: {current_price}")
+    chat_session = model.start_chat(
+        history=[
+            {
+                "role": "model",
+                "parts": [
+                    files[0],
+                    "15분 봉"
+                ],
+            },
+            {
+                "role": "model",
+                "parts": [
+                    files[1],
+                    "1시간 봉"
+                ],
+            },
+            {
+                "role": "user",
+                "parts": [
+                    files[2],
+                    f"4시간 봉",
+                ],
+            }
         ]
-
-        print(f"Current Price: {current_price}")
-        chat_session = model.start_chat(
-            history=[
-                {
-                    "role": "model",
-                    "parts": [
-                        files[0],
-                        "15분 봉"
-                    ],
-                },
-                {
-                    "role": "model",
-                    "parts": [
-                        files[1],
-                        "1시간 봉"
-                    ],
-                },
-                {
+    )
+    for attempt in range(3):
+        try:
+            if len(current_position) > 0:
+                response = chat_session.send_message(content={
                     "role": "user",
                     "parts": [
-                        files[2],
-                        f"4시간 봉",
+                        f"""포지션 사이드: {current_position[0]['info']['side']}
+                        진입 가격:{current_position[0]['entryPrice']}
+                        T/P:{current_position[0]['info']['takeProfit']}
+                        S/L:{current_position[0]['info']['stopLoss']}
+                        Current Price: {current_price}
+                        현재 hold,stop만 가능한 상황입니다.""",
                     ],
-                }
-            ]
-        )
-        if len(current_position)>0:
-            response = chat_session.send_message(content={
-                "role": "user",
-                "parts": [
-                    f"""포지션 사이드: {current_position[0]['info']['side']}
-                    진입 가격:{current_position[0]['entryPrice']}
-                    T/P:{current_position[0]['info']['takeProfit']}
-                    S/L:{current_position[0]['info']['stopLoss']}
-                    Current Price: {current_price}
-                    현재 hold,stop만 가능한 상황입니다.""",
-                ],
-            })
-        else:
-            response = chat_session.send_message(content={
-                "role": "user",
-                "parts": [
-                    f"""현재 포지션: 없음
-                    Current Price: {current_price}""",
-                ],
-            })
-        response = response.text
-        logging.info(response)
-        print(response)
+                })
+            else:
+                response = chat_session.send_message(content={
+                    "role": "user",
+                    "parts": [
+                        f"""현재 포지션: 없음
+                        Current Price: {current_price}""",
+                    ],
+                })
+            response = response.text
+            break
+        except Exception as e:
+            logging.error(f"Error sending message to chat session: {str(e)}")
+            if attempt < 2:
+                logging.info("Retrying...")
+            time.sleep(3)
+    logging.info(response)
+    print(response)
 
-        object = make_to_object()
-        value = object.make_it_object(response)
-        logging.info(value)
-        print(value)
-        
-        if value['stop_order'] == True:
-            bybit.close_position(value['Status'])
-            return
-        # 트레이딩 실행
-        if value['Status'] in ["buy", "sell"]:
-            side = value['Status']
-              
-            if value['buy_now'] == True:
-              typevalue = 'market'
-            elif value.get('buy_now') is None or value['buy_now'] == False:
-              typevalue = 'limit'
-              
-            position_params = Open_Position(
-                symbol="XRP/USDT:USDT",
-                type=typevalue,
-                price=value['price'],
-                side=side,
-                tp=value['tp'],
-                sl=value['sl'],
-                quantity=100
-            )
-            bybit.open_position(position_params)
-            logging.info(f"Position opened: {position_params}")
-            return
-        elif value['Status'] == "hold":
-            logging.info("No trading signal generated")
-            return
-    except Exception as e:
-        logging.error(f"Error in automation: {str(e)}")
+    object = make_to_object()
+    value = object.make_it_object(response)
+    logging.info(value)
+    print(value)
+    
+    if value['stop_order'] == True:
+        bybit.close_position(value['Status'])
+        return
+    # 트레이딩 실행
+    if value['Status'] in ["buy", "sell"]:
+        side = value['Status']
+            
+        if value['buy_now'] == True:
+            typevalue = 'market'
+        elif value.get('buy_now') is None or value['buy_now'] == False:
+            typevalue = 'limit'
+            
+        position_params = Open_Position(
+            symbol="XRP/USDT:USDT",
+            type=typevalue,
+            price=value['price'],
+            side=side,
+            tp=value['tp'],
+            sl=value['sl'],
+            quantity=100
+        )
+        bybit.open_position(position_params)
+        logging.info(f"Position opened: {position_params}")
+        return
+    elif value['Status'] == "hold":
+        logging.info("No trading signal generated")
+        return
 
 def run_scheduler():
     # 서울 시간대 설정
@@ -177,9 +182,11 @@ def run_scheduler():
     current_time = datetime.now(seoul_tz)
     logging.info(f"Scheduler started at {current_time}")
 
-    # 매 시간 00분 또는 30분에 실행
-    schedule.every().hour.at(":00").do(automation)
+    # 매 시간 15분 마다 실행행
+    schedule.every().hour.at(":15").do(automation)
     schedule.every().hour.at(":30").do(automation)
+    schedule.every().hour.at(":45").do(automation)
+    schedule.every().hour.at(":00").do(automation)
     
     # 매 15분마다 실행
     # schedule.every(15).minutes.do(automation)
